602b2bc104bd3e6f1ca417583b62dcfb967f6ce6
Liam C <liam@chaintail.xyz>
2024-05-30 23:18:47 -0400

    fix logo with HStack

--- ui/src/components/EvntFiLogo/EvntFiLogo.tsx
+++ ui/src/components/EvntFiLogo/EvntFiLogo.tsx
@@ -3,34 +3,22 @@ import { type FC } from "react";
 
 // import Link from "next/link";
 
-import { Stack, Box, Text, Link } from "@chakra-ui/react";
+import { Stack, Box, Text, Link, HStack } from "@chakra-ui/react";
 
 const EvntFiLogo: FC = () => {
   return (
-    // <Stack width="600px" height="206.25px" maxWidth="100%">
-    //   <Stack width="564px" height="206.25px" maxWidth="100%">
-    <Link
-      href="/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <Box as="footer" p={"1rem"} position="sticky" bottom={0} zIndex={10} textAlign={"center"}>
-        <Stack width="363.75px" height="165px" maxWidth="100%">
-          <Stack width="363.75px" height="165px" maxWidth="100%">
-            <Text fontFamily="Cutive Mono" fontWeight="regular" fontSize="150px" color="#000000">
-              evnt.
-            </Text>
-          </Stack>
-        </Stack>
-        <Stack width="98.25px" height="206.25px">
+    <Link href="/" target="_blank" rel="noopener noreferrer">
+      <Box as="footer" p={"0rem"} position="sticky" bottom={0} zIndex={10} textAlign={"center"}>
+        <HStack>
+          <Text fontFamily="Cutive Mono" fontWeight="regular" fontSize="150px" color="#000000">
+            evnt.
+          </Text>
           <Text fontFamily="Damion" fontWeight="regular" fontSize="150px" color="#000000">
             fi
           </Text>
-        </Stack>
+        </HStack>
       </Box>
     </Link>
-    //   </Stack>
-    // </Stack>
   );
 };
 
